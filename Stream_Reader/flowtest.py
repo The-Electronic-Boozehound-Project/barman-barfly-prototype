e0e2bcc17286bfca111ca6c47494c2dcfc10db52
Matthew Turner <mdtdev@users.noreply.github.com>
2021-01-03 13:39:02 -0500

    Initial commit of the reflow test program (not working)

--- Stream_Reader/flowtest.py
+++ Stream_Reader/flowtest.py
@@ -3,8 +3,23 @@
 import textwrap
 import os
 
+# Set up textwrappers and printers:
+
+narrow = textwrap.TextWrapper(width = 40)
+
+def line_printer(l):
+    """Prints a list of lines, line by line."""
+    
+
+
 # Open file
 
+with open('example_file.fly', 'r') as reader:
+     x = reader.readlines()
+
 # reflow
+l = narrow.wrap(x)
 
 #output
+for el in l:
+    print(el)
